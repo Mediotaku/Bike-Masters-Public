69b8864acf9910309d8bfc536d94bcc6b5f86bd8
Mediotaku <37799546+Mediotaku@users.noreply.github.com>
2020-01-12 23:38:34 +0100

    Add files via upload

---
+++ img/tiles4x8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles4x8" tilewidth="8" tileheight="8" tilecount="16" columns="4">
+ <image source="tiles4x8.png" width="32" height="32"/>
+</tileset>
